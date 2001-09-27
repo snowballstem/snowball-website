c546a17c97afe4b7732bc8e20152150d171959ce
Richard Boulton <richard@tartarus.org>
2001-09-27 01:37:51 +0000

    Adding initial version of website.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@2 633ccae0-01f4-0310-8c99-d3591da6f01f

---
+++ demo.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Snowball - Demo</title>
+</head>
+<body bgcolor=white>
+<table><tr><td valign=top>
+</td><td>
+<h1>Snowball</h1>
+</td></tr>
+<tr><td valign=top bgcolor=cyan>
+<?php include('menu.inc'); ?>
+</td><td valign=top>
+<p>
+
+<form method="POST" action="demo.php">
+Words to stem: <input name="words" size=30>
+</form>
+
+</p><p>
+
+Note: this is not yet functional.
+
+<?php echo "$words" ?>
+
+</p>
+</td></tr></table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Snowball</title>
+</head>
+<body bgcolor=white>
+<table><tr><td valign=top>
+</td><td>
+<h1>Snowball</h1>
+</td></tr>
+<tr><td valign=top bgcolor=cyan>
+<?php include('menu.inc'); ?>
+</td><td valign=top>
+<b>STATUS: (26 Sept 2001) initial upload of project is currently in progress.</b>
+<p>
+
+Snowball is a language for describing stemming algorithms, primarily for
+information retrieval.  Snowball scripts are converted to ANSI C code, which is
+then compiled to produce an extremely efficient stemming routine, capable of
+stemming upwards of one million words per second on a modern machine (eg, 1 GHz
+Pentium III).
+
+</p><p>
+
+Snowball scripts exist for performing stemming a number of European languages,
+together with equivalent C programs, stemmer definitions and sample
+vocabularies.
+
+</p><p>
+
+Snowball is released as free software under the BSD license, in the hope that
+it will be used in as wide a variety of situations as possible.  Developers are
+strongly encouraged to submit any and all comments, problem reports and
+improvements.
+
+</p>
+</td></tr></table>
+</body>
+</html>
