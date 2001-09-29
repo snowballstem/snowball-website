9061b026c70fbd6e344a8dde05b6648e98ea807a
Richard Boulton <richard@tartarus.org>
2001-09-29 12:11:38 +0000

    Add credits and lists pages
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@52 633ccae0-01f4-0310-8c99-d3591da6f01f

---
+++ credits.php
@@ -0,0 +1,20 @@
+<?php
+  require "menu.inc";
+  displayHeader("Credits");
+?>
+<p>
+
+Snowball, and all of the current stemming algorithms were written by
+Dr Martin Porter.
+
+</p><p>
+
+The project website and general administration was performed by Richard Boulton.
+
+</p><p>
+
+Website and project hosting is performed by
+<a href="http://sourceforge.net/">SourceForge</a>.
+
+</p>
+<?php displayFooter(); ?>
---
+++ lists.php
@@ -0,0 +1,27 @@
+<?php
+  require "menu.inc";
+  displayHeader("Mailing Lists");
+?>
+<p>
+
+There are two mailing lists related to Snowball:
+
+</p><p>
+
+<a href="http://lists.sourceforge.net/lists/listinfo/snowball-discuss">Snowball-discuss</a>
+is a list for general discussion of anything Snowball related.  Release
+announcements will also be posted to this list.
+
+</p><p>
+
+<a href="http://lists.sourceforge.net/lists/listinfo/snowball-commits">Snowball-commits</a>
+is a readonly list.  Whenever a change is made to Snowball, a message will
+automatically be sent to this list: it is therefore fairly high volume, and
+only of interest to people actively working on Snowball.
+
+</p><p>
+
+(You can subscribe to the lists by following the appropriate links above.)
+
+</p>
+<?php displayFooter(); ?>
