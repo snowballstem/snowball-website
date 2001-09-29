6abd993b8e31b8406b17675ab60f14e7957e7705
Richard Boulton <richard@tartarus.org>
2001-09-29 13:31:53 +0000

    Add a (content free) license page
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@53 633ccae0-01f4-0310-8c99-d3591da6f01f

---
+++ license.php
@@ -0,0 +1,14 @@
+<?php
+  require "menu.inc";
+  displayHeader("License");
+?>
+<p>
+
+Snowball is covered by the following license:
+
+</p><p>
+
+TO BE CONFIRMED BY MARTIN PORTER
+
+</p>
+<?php displayFooter(); ?>
