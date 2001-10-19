b1b91eb9eef1eb1891bcd5f05a43897e319e24fc
Richard Boulton <richard@tartarus.org>
2001-10-19 14:26:55 +0000

    Include text from the start.html page instead of our own pages.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@63 633ccae0-01f4-0310-8c99-d3591da6f01f

--- index.php
+++ index.php
@@ -21,28 +21,7 @@
       return $filecontents;
   }
 ?>
-<p>
-
-Snowball is a language for describing stemming algorithms, primarily for
-information retrieval.  Snowball scripts are converted to ANSI C code, which is
-then compiled to produce an extremely efficient stemming routine, capable of
-stemming upwards of one million words per second on a modern machine (eg, 1 GHz
-Pentium III).
-
-</p><p>
-
-Snowball scripts exist for performing stemming a number of European languages,
-together with equivalent C programs, stemmer definitions and sample
-vocabularies.
-
-</p><p>
-
-Snowball is released as free software under the BSD license, in the hope that
-it will be used in as wide a variety of situations as possible.  Developers are
-strongly encouraged to submit any and all comments, problem reports and
-improvements.
-
-</p>
+<?php include "texts/start.html" ?>
 <h2>Recent News</h2>
 <p>
 <?php
