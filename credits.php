25a365b8c4ee9892145d32889e118f8c41ad6003
Martin Porter <martin@tartarus.org>
2001-10-20 14:24:01 +0000

    New credits and License pages
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@66 633ccae0-01f4-0310-8c99-d3591da6f01f

--- credits.php
+++ credits.php
@@ -5,16 +5,26 @@
 <p>
 
 Snowball, and all of the current stemming algorithms were written by
-Dr Martin Porter.
+Dr Martin Porter, who also prepared the material for the Website. He wishes to
+thank Richard Boulton and Dr Andrew Macfarlane for technical and proofreading
+assistance.
 
 </p><p>
 
-The project website and general administration was performed by Richard Boulton.
+The website was established by Richard Boulton, who is still in charge of its
+administration.
 
 </p><p>
 
-Website and project hosting is performed by
-<a href="http://sourceforge.net/">SourceForge</a>.
+Linguistic assistance for Russian has been provided by Patrick Miles (of the
+Patrick Miles Translation Agency, Cambridge, UK). Pat is a distinguished
+translator, whose English versions of Chekhov have appeared on the London
+stage.
+</p><p>
+
+The website and project hosting is performed by
+<a href="http://sourceforge.net/">SourceForge</a>, to whom our grateful
+thanks are due.
 
 </p>
 <?php displayFooter(); ?>
--- license.php
+++ license.php
@@ -4,11 +4,26 @@
 ?>
 <p>
 
-Snowball is covered by the following license:
+All the software given out on this Snowball site is covered by the BSD License (see
+<A HREF="http://www.opensource.org/licenses/bsd-license.html">
+http://www.opensource.org/licenses/bsd-license.html </A>), with Copyright (c)
+2001, Dr Martin Porter.
+
 
 </p><p>
 
-TO BE CONFIRMED BY MARTIN PORTER
+Essentially, all this means is that you can do what you like with the code,
+except claim another Copyright for it, or claim that it is issued under a different
+license. The software is also issued without warranties, which means that if anyone
+suffers through its use, they cannot come back and sue you.
+You also have to alert anyone to whom you give the Snowball
+software to the fact that it is covered by the BSD license.
+
+</p><p>
+
+We have not bothered to insert the the licensing arrangement into the
+text of the Snowball softwre.
+
 
 </p>
 <?php displayFooter(); ?>
