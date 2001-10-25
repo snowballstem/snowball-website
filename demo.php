ecd6c0a7a4e6352110e9f53b224ed7f43524939a
Richard Boulton <richard@tartarus.org>
2001-10-25 00:31:23 +0000

    Moved various php functions out of demo.php and index.php and into menu.inc This puts all the complex php code into one file, leaving the php files reasonably clean and clear.
    Improved the robustness of the demo: extra newlines and spaces shouldn't
    confuse it now.
    
    
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@77 633ccae0-01f4-0310-8c99-d3591da6f01f

--- demo.php
+++ demo.php
@@ -2,29 +2,38 @@
   require "menu.inc";
   displayHeader("Demo");
 ?>
-<p>
+<TR><TD>
 
 <form method="POST" action="demo.php">
-Enter some (english) words to stem:<br>
-(Other languages will be added to the demo later)<br>
-<textarea name="words" rows="10" cols="50"><?php echo "$words" ?></textarea>
+Enter some words to stem, using the English stemming algorithm, and then click on Submit:<br>
+<textarea name="words" rows="10" cols="50"><?php
+$tmp = preg_replace('|\\\\|', '', $words);
+echo "$tmp";
+?></textarea>
 <br>
 <input type="submit" name="Submit" value="Submit">
 </form>
-</p>
+</TD></TR>
 
 <?php
 if ($words != '')
 {
-  echo "<pre>";
+  echo "<TR><TD>";
+  echo "<h2>Results</h2><pre>";
   $tmpname = tempnam("/tmp", "snowball_stemdemo");
   $tmpfile = fopen($tmpname, "w");
+  $words = strtolower($words);
+  $words = preg_replace('|[^-A-Za-z\']|', ' ', $words);
+  $words = preg_replace('|[-\']|', '', $words);
+  $words = preg_replace('|  *|', ' ', $words);
+  $words = preg_replace('| *$|', '', $words);
   # Have a limit of 10000 bytes, just in case.
   fwrite($tmpfile, $words, 10000);
   fclose($tmpfile);
   passthru ("./stemdemo_en $tmpname");
   unlink($tmpname);
   echo "</pre>";
+  echo "</TR></TD>";
 }
 ?>
 
--- index.php
+++ index.php
@@ -1,34 +1,16 @@
 <?php
   require "menu.inc";
-  displayHeader("");
-
-  function get_news() {
-      $filehandle = fopen ("http://sourceforge.net/export/projnews.php?group_id=36391&limit=5&flat=1&show_summaries=1", "r");
-      if (!$filehandle) {
-	  return "Unable to open remote file.";
-      } else {
-	  $filecontents = '';
-	  while (!feof ($filehandle)) {
-	      $filecontents .= fgets ($filehandle, 1024);
-	  }
-	  fclose ($filehandle);
-      }
-
-      $filecontents = preg_replace('|<I>[^<]*</I>[^<]*<A HREF="http://sourceforge.net/projects/snowball/">Snowball</A>|', '', $filecontents);
-      $filecontents = preg_replace('|<div align="center">.*?</div>|', '', $filecontents);
-      $filecontents = preg_replace('|<HR.*?>|i', '', $filecontents);
-
-      return $filecontents;
-  }
+  displayHeader("Introduction");
 ?>
+
 <?php include "index_body.html" ?>
+
 <TR><TD>
 <p>
 <h2>Recent News</h2>
 <p>
-<?php
-  echo get_news();
-?>
+<?php display_news(); ?>
 </p>
 </TR></TD>
+
 <?php displayFooter(); ?>
