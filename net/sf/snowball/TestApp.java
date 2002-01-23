fcd3e95a7468e4eeacb2a6dd0f6053a3f4f62fd5
Richard Boulton <richard@tartarus.org>
2002-01-23 20:36:26 +0000

    Attempt to make output buffered, and allow tests which repeat the stemming several times.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@135 633ccae0-01f4-0310-8c99-d3591da6f01f

--- net/sf/snowball/TestApp.java
+++ net/sf/snowball/TestApp.java
@@ -3,6 +3,9 @@ package net.sf.snowball;
 
 import net.sf.snowball.ext.EnglishStemmer;
 import java.io.FileReader;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
 
 public class TestApp {
     public static void main(String [] args) throws Throwable {
@@ -11,14 +14,32 @@ public class TestApp {
 	FileReader reader = new FileReader(args[0]);
 	StringBuffer input = new StringBuffer();
 
+        OutputStream output;
+
+	if (args.length > 2 && args[1].equals("-o")) {
+	    output = new FileOutputStream(args[2]);
+	    output = new BufferedOutputStream(output);
+	} else if (args.length == 2) {
+	     System.err.println("Usage: TestApp <input file> [-o <output file>]");
+	     return;
+	} else {
+	    output = System.out;
+	}
+	int repeat = 1;
+	if (args.length > 3) {
+	    repeat = Integer.parseInt(args[3]);
+	}
+
 	while (reader.ready()) {
 	    char ch = (char) reader.read();
 	    if (Character.isWhitespace(ch)) {
 		if (input.length() > 0) {
 		    //System.out.println("Stemming '" + input + "'");
 		    stemmer.setCurrent(input.toString());
-		    stemmer.stem();
-		    System.out.println(stemmer.current);
+		    for (int i = repeat; i != 0; i--) {
+			stemmer.stem();
+		    }
+		    output.write(stemmer.current.toString().getBytes());
 		    input.delete(0, input.length());
 		}
 	    } else {
