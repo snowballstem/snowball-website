01a5c847b4d72d307d75df7e4889ab23c82a6d6b
Richard Boulton <richard@tartarus.org>
2002-01-23 22:35:13 +0000

    Add pystemmer link.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@136 633ccae0-01f4-0310-8c99-d3591da6f01f

--- projects.php
+++ projects.php
@@ -5,13 +5,17 @@
 <TR><TD>
 <p>
 
-This page will list projects which make use of snowball.
-However, as yet, we know of no such projects.
+This page lists projects which make use of snowball.
 
-</p><p>
+<hr>
+
+<a href="http://www.sourceforge.net/projects/pystemmer/"><h2>PyStemmer</a></h2>
+A python interface to snowball.
+
+</p>
+<hr>
 <a href="mailto:snowball-discuss@lists.sourceforge.net">Contact us</a> if
 you have a project that you would like to be listed here.
 
-</p>
 <TD><TR>
 <?php displayFooter(); ?>
