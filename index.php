9388ef55c840266bf22e3d7b0dd4b8f415bcc3e9
Richard Boulton <richard@tartarus.org>
2002-02-18 13:56:39 +0000

    Remove automatically generated news, and replace with manually inserted news items.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@147 633ccae0-01f4-0310-8c99-d3591da6f01f

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
 <p>
 <h2>Recent News</h2>
 <p>
-<?php display_news(); ?>
+<b>Feb 18, 2002 - Java support added</b>
+<br>
+Richard has modified the snowball code generator to produce Java output as
+well as ANSI C output.  This means that pure Java systems can now use the
+snowball stemmers.
 </p>
 </TR></TD>
 
