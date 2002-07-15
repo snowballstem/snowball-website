2be1f09263372bfd582e90d56988719760e0544a
Martin Porter <martin@tartarus.org>
2002-07-15 09:45:33 +0000

    Lovins stemming algorithm
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@169 633ccae0-01f4-0310-8c99-d3591da6f01f

---
+++ lovins/stem.h
@@ -0,0 +1,8 @@
+
+/* This file was generated automatically by the Snowball to ANSI C compiler */
+
+extern struct SN_env * create_env(void);
+extern void close_env(struct SN_env * z);
+
+extern int stem(struct SN_env * z);
+
