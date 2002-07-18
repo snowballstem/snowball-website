ece2f1908675a163122167b985c7ccc33310f90a
Martin Porter <martin@tartarus.org>
2002-07-18 08:18:07 +0000

    Announcements and site history brought up-to-date
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@173 633ccae0-01f4-0310-8c99-d3591da6f01f

--- index.php
+++ index.php
@@ -7,14 +7,35 @@
 
 <TR><TD>
 <p>
-<h2>Recent News</h2>
+<h2>Major events</h2>
+
 <p>
-<b>Feb 18, 2002 - Java support added</b>
+<b>Announcement</b>
+<br>
+Martin Porter is developing a Finnish stemmer. Email him if you feel
+you have an interest (<tt>martin@tartarus.org</tt>).
+</p>
+
+<p>
+<b>July 2002 - ISO Latin I as default</b>
+<br>
+The use of MS DOS Latin I is now history, but the old versions of the
+Snowball stemmers are still accessible on the site.
+</p>
+
+<p>
+<b>May 2002 - Unicode support added</b>
+<br>
+</p>
+
+<p>
+<b>Feb 2002 - Java support added</b>
 <br>
 Richard has modified the snowball code generator to produce Java output as
 well as ANSI C output.  This means that pure Java systems can now use the
 snowball stemmers.
 </p>
+
 </TR></TD>
 
 <?php displayFooter(); ?>
