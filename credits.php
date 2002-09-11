b78fb3ed53e7fb655702e8d8c76f9b715fa46ab3
Martin Porter <martin@tartarus.org>
2002-09-11 10:53:49 +0000

    New credits
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@183 633ccae0-01f4-0310-8c99-d3591da6f01f

--- credits.php
+++ credits.php
@@ -4,7 +4,6 @@
 ?>
 <TR><TD>
 <p>
-
 Snowball, and all of the current stemming algorithms were written by
 Dr Martin Porter, who also prepared the material for the Website.
 The Snowball to Java codegenerator, and supporting Java libraries, were
@@ -12,24 +11,31 @@ contributed by Richard Boulton.
 Dr Andrew Macfarlane, of City University, London, gave much
 initial encouragement and proofreading
 assistance.
+</p>
 
-</p><p>
-
+<p>
 The website was established by Richard Boulton, who is still in charge of its
 administration.
+</p>
 
-</p><p>
-
+<p>
 Linguistic assistance for Russian has been provided by Patrick Miles (of the
 Patrick Miles Translation Agency, Cambridge, UK). Pat is a distinguished
 translator, whose English versions of Chekhov have appeared on the London
 stage.
-</p><p>
-
-The website and project hosting is performed by
-<a href="http://sourceforge.net/">SourceForge</a>, to whom our grateful
-thanks are due.
-
 </p>
+
+<p>
+Various emailers have helped improve the stemmers with their many suggestions
+and comments. We must especially mention Andrei Aksyonoff and
+Oleg Bartunov (Russian), Steve Tolkin (English), and Fred Brault (French).
+</p>
+
+<p>
+The Snowball site is provided to us free by James Aylett, who
+owns and runs the machine that hosts the
+<a href="http://www.tartarus.org/">tartarus</a> website.
+</p>
+
 </TD></TR>
 <?php displayFooter(); ?>
