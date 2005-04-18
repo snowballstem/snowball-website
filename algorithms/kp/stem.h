c1e450ddf3a97f25f74d3ad35eca84189ff63226
Richard Boulton <richard@tartarus.org>
2005-04-18 11:10:13 +0000

    Remove more old files.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@280 633ccae0-01f4-0310-8c99-d3591da6f01f

--- algorithms/kp/stem.h
+++
@@ -1,8 +0,0 @@
-
-/* This file was generated automatically by the Snowball to ANSI C compiler */
-
-extern struct SN_env * kp_create_env(void);
-extern void kp_close_env(struct SN_env * z);
-
-extern int kp_stem(struct SN_env * z);
-
--- algorithms/lovins/stem.h
+++
@@ -1,8 +0,0 @@
-
-/* This file was generated automatically by the Snowball to ANSI C compiler */
-
-extern struct SN_env * lovins_create_env(void);
-extern void lovins_close_env(struct SN_env * z);
-
-extern int lovins_stem(struct SN_env * z);
-
