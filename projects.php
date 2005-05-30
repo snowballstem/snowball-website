1e401c5684b8667c5a791f5bfdadfe73de8b3609
Richard Boulton <richard@tartarus.org>
2005-05-30 10:20:49 +0000

    Mention Xapian and TextIndexNG
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/website@339 633ccae0-01f4-0310-8c99-d3591da6f01f

--- projects.php
+++ projects.php
@@ -17,6 +17,15 @@ A python interface to snowball.
 A reimplementation of the snowball stemming algorithms in C++.
 </p>
 
+
+<h2><a href="http://www.xapian.org/">Xapian</a></h2>
+A probabilistic search engine which supports stemming using Snowball.
+</p>
+
+<h2><a href="http://www.zopyx.com/OpenSource/TextIndexNG">TextIndexNG</a></h2>
+A fulltext indexing solution for Zope with supports stemming using Snowball.
+</p>
+
 <hr>
 <a href="mailto:snowball-discuss@lists.tartarus.org">Contact us</a> if
 you have a project that you would like to be listed here.
