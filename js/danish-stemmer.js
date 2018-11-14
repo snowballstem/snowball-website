306f4e04bd6cb21e62f2a31976bab50f705c0759
Olly Betts <olly@survex.com>
2018-11-15 12:32:45 +1300

    Update Danish stemmer for alphanumeric change
    See https://github.com/snowballstem/snowball/issues/81

--- js/danish-stemmer.js
+++ js/danish-stemmer.js
@@ -54,6 +54,8 @@ function DanishStemmer() {
         ["l\u00F8st", -1, 2]
     ];
 
+    /** @const */ var /** Array<int> */ g_c = [119, 223, 119, 1];
+
     /** @const */ var /** Array<int> */ g_v = [17, 65, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 0, 128];
 
     /** @const */ var /** Array<int> */ g_s_ending = [239, 254, 42, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16];
@@ -65,12 +67,12 @@ function DanishStemmer() {
 
     /** @return {boolean} */
     function r_mark_regions() {
-        // (, line 29
+        // (, line 31
         I_p1 = base.limit;
-        // test, line 33
+        // test, line 35
         var /** number */ v_1 = base.cursor;
-        // (, line 33
-        // hop, line 33
+        // (, line 35
+        // hop, line 35
         {
             var /** number */ c1 = base.cursor + 3;
             if (0 > c1 || c1 > base.limit)
@@ -79,10 +81,10 @@ function DanishStemmer() {
             }
             base.cursor = c1;
         }
-        // setmark x, line 33
+        // setmark x, line 35
         I_x = base.cursor;
         base.cursor = v_1;
-        // goto, line 34
+        // goto, line 36
         golab0: while(true)
         {
             var /** number */ v_2 = base.cursor;
@@ -101,7 +103,7 @@ function DanishStemmer() {
             }
             base.cursor++;
         }
-        // gopast, line 34
+        // gopast, line 36
         golab2: while(true)
         {
             lab3: {
@@ -117,11 +119,11 @@ function DanishStemmer() {
             }
             base.cursor++;
         }
-        // setmark p1, line 34
+        // setmark p1, line 36
         I_p1 = base.cursor;
-        // try, line 35
+        // try, line 37
         lab4: {
-            // (, line 35
+            // (, line 37
             if (!(I_p1 < I_x))
             {
                 break lab4;
@@ -134,47 +136,43 @@ function DanishStemmer() {
     /** @return {boolean} */
     function r_main_suffix() {
         var /** number */ among_var;
-        // (, line 40
-        // setlimit, line 41
-        var /** number */ v_1 = base.limit - base.cursor;
-        // tomark, line 41
+        // (, line 42
+        // setlimit, line 43
         if (base.cursor < I_p1)
         {
             return false;
         }
-        base.cursor = I_p1;
         var /** number */ v_2 = base.limit_backward;
-        base.limit_backward = base.cursor;
-        base.cursor = base.limit - v_1;
-        // (, line 41
-        // [, line 41
+        base.limit_backward = I_p1;
+        // (, line 43
+        // [, line 43
         base.ket = base.cursor;
-        // substring, line 41
+        // substring, line 43
         among_var = base.find_among_b(a_0);
         if (among_var == 0)
         {
             base.limit_backward = v_2;
             return false;
         }
-        // ], line 41
+        // ], line 43
         base.bra = base.cursor;
         base.limit_backward = v_2;
         switch (among_var) {
             case 1:
-                // (, line 48
-                // delete, line 48
+                // (, line 50
+                // delete, line 50
                 if (!base.slice_del())
                 {
                     return false;
                 }
                 break;
             case 2:
-                // (, line 50
+                // (, line 52
                 if (!(base.in_grouping_b(g_s_ending, 97, 229)))
                 {
                     return false;
                 }
-                // delete, line 50
+                // delete, line 52
                 if (!base.slice_del())
                 {
                     return false;
@@ -186,43 +184,39 @@ function DanishStemmer() {
 
     /** @return {boolean} */
     function r_consonant_pair() {
-        // (, line 54
-        // test, line 55
+        // (, line 56
+        // test, line 57
         var /** number */ v_1 = base.limit - base.cursor;
-        // (, line 55
-        // setlimit, line 56
-        var /** number */ v_2 = base.limit - base.cursor;
-        // tomark, line 56
+        // (, line 57
+        // setlimit, line 58
         if (base.cursor < I_p1)
         {
             return false;
         }
-        base.cursor = I_p1;
         var /** number */ v_3 = base.limit_backward;
-        base.limit_backward = base.cursor;
-        base.cursor = base.limit - v_2;
-        // (, line 56
-        // [, line 56
+        base.limit_backward = I_p1;
+        // (, line 58
+        // [, line 58
         base.ket = base.cursor;
-        // substring, line 56
+        // substring, line 58
         if (base.find_among_b(a_1) == 0)
         {
             base.limit_backward = v_3;
             return false;
         }
-        // ], line 56
+        // ], line 58
         base.bra = base.cursor;
         base.limit_backward = v_3;
         base.cursor = base.limit - v_1;
-        // next, line 62
+        // next, line 64
         if (base.cursor <= base.limit_backward)
         {
             return false;
         }
         base.cursor--;
-        // ], line 62
+        // ], line 64
         base.bra = base.cursor;
-        // delete, line 62
+        // delete, line 64
         if (!base.slice_del())
         {
             return false;
@@ -233,68 +227,64 @@ function DanishStemmer() {
     /** @return {boolean} */
     function r_other_suffix() {
         var /** number */ among_var;
-        // (, line 65
-        // do, line 66
+        // (, line 67
+        // do, line 68
         var /** number */ v_1 = base.limit - base.cursor;
         lab0: {
-            // (, line 66
-            // [, line 66
+            // (, line 68
+            // [, line 68
             base.ket = base.cursor;
-            // literal, line 66
+            // literal, line 68
             if (!(base.eq_s_b("st")))
             {
                 break lab0;
             }
-            // ], line 66
+            // ], line 68
             base.bra = base.cursor;
-            // literal, line 66
+            // literal, line 68
             if (!(base.eq_s_b("ig")))
             {
                 break lab0;
             }
-            // delete, line 66
+            // delete, line 68
             if (!base.slice_del())
             {
                 return false;
             }
         }
         base.cursor = base.limit - v_1;
-        // setlimit, line 67
-        var /** number */ v_2 = base.limit - base.cursor;
-        // tomark, line 67
+        // setlimit, line 69
         if (base.cursor < I_p1)
         {
             return false;
         }
-        base.cursor = I_p1;
         var /** number */ v_3 = base.limit_backward;
-        base.limit_backward = base.cursor;
-        base.cursor = base.limit - v_2;
-        // (, line 67
-        // [, line 67
+        base.limit_backward = I_p1;
+        // (, line 69
+        // [, line 69
         base.ket = base.cursor;
-        // substring, line 67
+        // substring, line 69
         among_var = base.find_among_b(a_2);
         if (among_var == 0)
         {
             base.limit_backward = v_3;
             return false;
         }
-        // ], line 67
+        // ], line 69
         base.bra = base.cursor;
         base.limit_backward = v_3;
         switch (among_var) {
             case 1:
-                // (, line 70
-                // delete, line 70
+                // (, line 72
+                // delete, line 72
                 if (!base.slice_del())
                 {
                     return false;
                 }
-                // do, line 70
+                // do, line 72
                 var /** number */ v_4 = base.limit - base.cursor;
                 lab1: {
-                    // call consonant_pair, line 70
+                    // call consonant_pair, line 72
                     if (!r_consonant_pair())
                     {
                         break lab1;
@@ -303,8 +293,8 @@ function DanishStemmer() {
                 base.cursor = base.limit - v_4;
                 break;
             case 2:
-                // (, line 72
-                // <-, line 72
+                // (, line 74
+                // <-, line 74
                 if (!base.slice_from("l\u00F8s"))
                 {
                     return false;
@@ -316,41 +306,37 @@ function DanishStemmer() {
 
     /** @return {boolean} */
     function r_undouble() {
-        // (, line 75
-        // setlimit, line 76
-        var /** number */ v_1 = base.limit - base.cursor;
-        // tomark, line 76
+        // (, line 77
+        // setlimit, line 78
         if (base.cursor < I_p1)
         {
             return false;
         }
-        base.cursor = I_p1;
         var /** number */ v_2 = base.limit_backward;
-        base.limit_backward = base.cursor;
-        base.cursor = base.limit - v_1;
-        // (, line 76
-        // [, line 76
+        base.limit_backward = I_p1;
+        // (, line 78
+        // [, line 78
         base.ket = base.cursor;
-        if (!(base.out_grouping_b(g_v, 97, 248)))
+        if (!(base.in_grouping_b(g_c, 98, 122)))
         {
             base.limit_backward = v_2;
             return false;
         }
-        // ], line 76
+        // ], line 78
         base.bra = base.cursor;
-        // -> ch, line 76
+        // -> ch, line 78
         S_ch = base.slice_to();
         if (S_ch == '')
         {
             return false;
         }
         base.limit_backward = v_2;
-        // name ch, line 77
+        // name ch, line 79
         if (!(base.eq_s_b(S_ch)))
         {
             return false;
         }
-        // delete, line 78
+        // delete, line 80
         if (!base.slice_del())
         {
             return false;
@@ -359,54 +345,54 @@ function DanishStemmer() {
     };
 
     this.stem = /** @return {boolean} */ function() {
-        // (, line 82
-        // do, line 84
+        // (, line 84
+        // do, line 86
         var /** number */ v_1 = base.cursor;
         lab0: {
-            // call mark_regions, line 84
+            // call mark_regions, line 86
             if (!r_mark_regions())
             {
                 break lab0;
             }
         }
         base.cursor = v_1;
-        // backwards, line 85
+        // backwards, line 87
         base.limit_backward = base.cursor; base.cursor = base.limit;
-        // (, line 85
-        // do, line 86
+        // (, line 87
+        // do, line 88
         var /** number */ v_2 = base.limit - base.cursor;
         lab1: {
-            // call main_suffix, line 86
+            // call main_suffix, line 88
             if (!r_main_suffix())
             {
                 break lab1;
             }
         }
         base.cursor = base.limit - v_2;
-        // do, line 87
+        // do, line 89
         var /** number */ v_3 = base.limit - base.cursor;
         lab2: {
-            // call consonant_pair, line 87
+            // call consonant_pair, line 89
             if (!r_consonant_pair())
             {
                 break lab2;
             }
         }
         base.cursor = base.limit - v_3;
-        // do, line 88
+        // do, line 90
         var /** number */ v_4 = base.limit - base.cursor;
         lab3: {
-            // call other_suffix, line 88
+            // call other_suffix, line 90
             if (!r_other_suffix())
             {
                 break lab3;
             }
         }
         base.cursor = base.limit - v_4;
-        // do, line 89
+        // do, line 91
         var /** number */ v_5 = base.limit - base.cursor;
         lab4: {
-            // call undouble, line 89
+            // call undouble, line 91
             if (!r_undouble())
             {
                 break lab4;
